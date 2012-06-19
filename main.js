d883da2bbf295a28cadfe395913ddc14d504a83a
Stu Rob <stumrob@gmail.com>
2012-06-19 14:31:26 +0100

    fix fucking maps 404

--- main.js
+++ main.js
@@ -42,7 +42,7 @@
 
     window.v.map = new L.Map('map');
 
-    var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/65492/256/{z}/{x}/{y}.png',
+    var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/65711/256/{z}/{x}/{y}.png',
         cloudmadeAttribution = '©2012 OSM, CloudMade + foursquare',
         cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18,  detectRetina: true, attribution: cloudmadeAttribution});
 
